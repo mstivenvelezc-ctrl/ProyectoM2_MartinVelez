4b7dc4a0691bbd76515938f4d4fb081063208d11
stiven <mstivenvelezc@gmail.com>
2026-04-20 09:16:20 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'; 
 import express from "express";
+import cors from "cors";
 import router from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHanlder.js";
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
