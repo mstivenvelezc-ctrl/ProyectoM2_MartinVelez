85622401da7f42ac197f4aee64fb54a8b9d5e009
stiven <mstivenvelezc@gmail.com>
2026-04-21 07:54:45 -0500

    coneccion con BD railway

--- src/db/config.js
+++ src/db/config.js
@@ -6,13 +6,7 @@ const { Pool } = pg;
 dotenv.config();
 
 const pool = new Pool({
-  //connectionString: process.env.DATABASE_URL,
-
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+connectionString: process.env.DATABASE_URL
 });
 
 export default pool;
